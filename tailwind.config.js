c1105738aade94b4fddd94593e7c20bf56eb5849
Dimasiktut <schckudnov.dima@gmail.com>
2025-05-29 15:25:31 +0500

    Обновления: правки App.tsx и .gitignore

---
+++ tailwind.config.js
@@ -0,0 +1,13 @@
+// tailwind.config.js
+module.exports = {
+  content: [
+    "./index.html",
+    "./index.tsx",
+    "./components/**/*.{js,ts,jsx,tsx}",
+    "./screens/**/*.{js,ts,jsx,tsx}",
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
